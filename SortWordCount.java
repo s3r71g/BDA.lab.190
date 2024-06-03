714cd9975e3cf6bdf67593f65adcfef6599ea1cd
s3r71g <96942361+s3r71g@users.noreply.github.com>
2024-06-03 05:46:12 +0530

    Create SortWordCount.java

---
+++ SortWordCount.java
@@ -0,0 +1,12 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class SortWordCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job =
