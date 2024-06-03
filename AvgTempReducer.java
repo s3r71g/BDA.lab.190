b3ca142722ee13ed8fcf1046cae05d54e0999833
s3r71g <96942361+s3r71g@users.noreply.github.com>
2024-06-03 05:41:03 +0530

    Create AvgTempReducer.java

---
+++ AvgTempReducer.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class AvgTempReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        int count = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+            count++;
+        }
+        int avg = sum / count;
+        context.write(key, new IntWritable(avg));
+    }
+}
