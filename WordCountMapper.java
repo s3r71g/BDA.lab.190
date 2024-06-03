6bc1d4d7a7ab8d6e0e5befd414dcd0bec2031cbd
s3r71g <96942361+s3r71g@users.noreply.github.com>
2024-06-03 05:37:48 +0530

    Create WordCountMapper.java

---
+++ WordCountMapper.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] tokens = value.toString().split("\\s+");
+        for (String token : tokens) {
+            word.set(token);
+            context.write(word, one);
+        }
+    }
+}
+
