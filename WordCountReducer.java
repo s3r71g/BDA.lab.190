9d3ccca528e8b54ee3f9281876d008b6146c0496
s3r71g <96942361+s3r71g@users.noreply.github.com>
2024-06-03 05:58:58 +0530

    WordCount Program 6

--- WordCountReducer.java
+++ WordCountReducer.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private IntWritable result = new IntWritable();
 
