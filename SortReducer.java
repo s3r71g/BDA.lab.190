35ee355c709a9de3635dafb1eb84b5733635ffdc
s3r71g <96942361+s3r71g@users.noreply.github.com>
2024-06-03 05:53:39 +0530

    sort content alphabetically 8

--- SortReducer.java
+++ SortReducer.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+
 public class SortReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private PriorityQueue<WordCountPair> queue = new PriorityQueue<>(10, (a, b) -> b.count - a.count);
 
